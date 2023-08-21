14cf2247efbe48b0142d474c96c3661279e5ae6f
Woganog <115092262+YKW-Modding@users.noreply.github.com>
2023-08-21 20:23:53 +0100

    GUI version
    yes

---
+++ main_cmd.py
@@ -0,0 +1,97 @@
+import mmap
+import os
+import sys
+
+
+def main():
+	save = input("Save file: ")
+	print("Set any of the following to 'skip' to not edit it.")
+	map = input("Map code: ")
+	
+	if len(map) != 7 and map != "skip":
+		print("Too long for a map ID!")
+		main()
+	
+	xCoordIn = input("X Coordinate: ")
+	if xCoordIn != "skip":
+		try:
+			xCoord = int(xCoordIn)
+		except ValueError:
+			print("Not an integer!")
+			main()
+	yCoordIn = input("Y Coordinate: ")
+	if yCoordIn != "skip":
+		try:
+			yCoord = int(yCoordIn)
+		except ValueError:
+			print("Not an integer!")
+			main()
+	zCoordIn = input("Z Coordinate: ")
+	if zCoordIn != "skip":
+		try:
+			zCoord = int(zCoordIn)
+		except ValueError:
+			print("Not an integer!")
+			main()
+	
+	timeIn = input("Time from -32767 to 32767: ")
+	if timeIn != "skip":
+		try:
+			time = int(timeIn)
+		except ValueError:
+			print("Not an integer!")
+			main()
+		if not -32767 <= time <= 32767 and timeIn != "skip":
+			print("Number is too large!")
+			main()
+	
+	sunTimeIn = input("Sun time (0 for midnight-midday and 1 for midday-midnight): ")
+	if sunTimeIn != "skip":
+		try:
+			sunTime = int(sunTimeIn)
+		except ValueError:
+			print("Not an integer!")
+			main()
+		if sunTime != 0 and sunTime != 1:
+			print("Not 0 or 1!")
+			main()
+	
+	keepDecrypted = input("Keep decrypted save file? (y/n): ")
+	if keepDecrypted != "y" and keepDecrypted != "n":
+		print("Not 'y' or 'n'!")
+		main()
+	
+	os.system("python yw_save/yw_save.py --game yw --decrypt " + save + " decrypted.yw")
+	
+	with open("decrypted.yw", "r+b") as f:
+		if map != "skip":
+			f.seek(72)
+			f.write(bytes(map, 'utf-8'))
+		if xCoordIn != "skip":
+			f.seek(20)
+			f.write(xCoord.to_bytes(4, byteorder = "little", signed = True))
+		if yCoordIn != "skip":
+			f.seek(24)
+			f.write(yCoord.to_bytes(4, byteorder = "little", signed = True))
+		if zCoordIn != "skip":
+			f.seek(28)
+			f.write(zCoord.to_bytes(4, byteorder = "little", signed = True))
+		if timeIn != "skip":
+			f.seek(1488)
+			f.write(time.to_bytes(2, byteorder = "little", signed = True))
+		if sunTimeIn != "skip":
+			f.seek(1490)
+			if sunTime == 0:
+				f.write(int.to_bytes(1, 1, byteorder = "little"))
+			if sunTime == 1:
+				f.write(int.to_bytes(2, 1, byteorder = "little"))
+			
+	os.system("python yw_save/yw_save.py --game yw --encrypt decrypted.yw output.yw")
+	
+	if keepDecrypted == "n":
+		os.remove("decrypted.yw")
+	
+	input("Complete! File saved as output.yw. Press Enter to continue... ")
+	sys.exit()
+
+main()
---
+++ main_gui.py
@@ -0,0 +1,105 @@
+from PyQt5 import QtWidgets, uic, QtGui
+import sys
+import os
+
+class Ui(QtWidgets.QMainWindow):
+	def __init__(self):
+		super(Ui, self).__init__()
+		uic.loadUi('gui.ui', self)
+		
+		browseFileName = self.findChild(QtWidgets.QPushButton, "browseFileName")
+		browseFileName.clicked.connect(self.browseSaveFile)
+		
+		mapCodeLink = self.findChild(QtWidgets.QLabel, "mapCodeLink")
+		mapCodeLink.setText('Map codes can be found <a href="https://tcrf.net/Notes:Yo-kai_Watch_(Nintendo_3DS)#Map_names">here!</a>')
+		
+		onlyInt = QtGui.QIntValidator()
+		onlyInt.setRange(-2147483647, 2147483647)
+		
+		global xCoordText
+		xCoordText = self.findChild(QtWidgets.QLineEdit, "xCoordText")
+		xCoordText.setValidator(onlyInt)
+		
+		global yCoordText
+		yCoordText = self.findChild(QtWidgets.QLineEdit, "yCoordText")
+		yCoordText.setValidator(onlyInt)
+		
+		global zCoordText
+		zCoordText = self.findChild(QtWidgets.QLineEdit, "zCoordText")
+		zCoordText.setValidator(onlyInt)
+		
+		onlyIntTime = QtGui.QIntValidator()
+		onlyIntTime.setRange(-32767, 32767)
+		
+		global timeText
+		timeText = self.findChild(QtWidgets.QLineEdit, "timeText")
+		timeText.setValidator(onlyIntTime)
+		
+		global mapText
+		mapText = self.findChild(QtWidgets.QLineEdit, "mapText")
+		mapText.setMaxLength(7)
+		
+		goButton = self.findChild(QtWidgets.QPushButton, "goButton")
+		goButton.clicked.connect(self.goFunction)
+		
+		global sunTimeComboBox
+		sunTimeComboBox = self.findChild(QtWidgets.QComboBox, "sunTimeComboBox")
+		
+		global saveFileName
+		saveFileName = self.findChild(QtWidgets.QLineEdit, "saveFileName")
+		
+		global keepDecryptedCheckBox
+		keepDecryptedCheckBox = self.findChild(QtWidgets.QCheckBox, "keepDecryptedCheckBox")
+		
+		self.show()
+	
+	def browseSaveFile(self):
+		fname = QtWidgets.QFileDialog.getOpenFileName(self, "Open save file", "C:", "Yo-kai Watch save files (*.yw)")
+		self.saveFileName.setText(fname[0])
+	
+	def successPopup(self):
+		self.popup = QtWidgets.QMessageBox()
+		self.popup.setWindowTitle("Done!")
+		self.popup.setDefaultButton(QtWidgets.QMessageBox.Ok)
+		self.popup.setText("Complete! File saved as output.yw.")
+		
+		self.popup.show()
+	
+	def goFunction(self):
+		os.system('python yw_save/yw_save.py --game yw --decrypt "' + saveFileName.text() + '" decrypted.yw')
+		
+		with open("decrypted.yw", "r+b") as f:
+			if mapText.text() != "":
+				f.seek(72)
+				f.write(bytes(mapText.text(), 'utf-8'))
+			if xCoordText.text() != "":
+				xCoord = int(xCoordText.text())
+				f.seek(20)
+				f.write(xCoord.to_bytes(4, byteorder = "little", signed = True))
+			if yCoordText.text() != "":
+				yCoord = int(yCoordText.text())
+				f.seek(24)
+				f.write(yCoord.to_bytes(4, byteorder = "little", signed = True))
+			if zCoordText.text() != "":
+				zCoord = int(zCoordText.text())
+				f.seek(28)
+				f.write(zCoord.to_bytes(4, byteorder = "little", signed = True))
+			if timeText.text() != "":
+				time = int(timeText.text())
+				f.seek(1488)
+				f.write(time.to_bytes(2, byteorder = "little", signed = True))
+			f.seek(1490)
+			if sunTimeComboBox.currentText() == "Midnight to Midday":
+				f.write(int.to_bytes(1, 1, byteorder = "little"))
+			if sunTimeComboBox.currentText() == "Midday to Midnight":
+				f.write(int.to_bytes(2, 1, byteorder = "little"))
+		
+		os.system("python yw_save/yw_save.py --game yw --encrypt decrypted.yw output.yw")
+		if keepDecryptedCheckBox.isChecked() == False:
+			os.remove("decrypted.yw")
+		
+		self.successPopup()
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
